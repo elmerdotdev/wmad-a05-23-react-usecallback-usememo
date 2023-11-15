9e31912cf60b437754b09fb3436834528205e64c
Elmer Balbin <contact@elmer.dev>
2023-11-15 15:15:07 -0800

    usecallback usememo code along

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react"
+import { useCallback, useMemo, useState } from "react"
 import Welcome from "./components/Welcome"
 
 const App = () => {
@@ -8,20 +8,24 @@ const App = () => {
   const slowFunction = (num) => {
     console.log("slow function ran!")
     let startTime = performance.now()
-    while (performance.now() - startTime < 5000) {
+    while (performance.now() - startTime < 500) {
       //
     }
     return Math.random() * num
   }
 
   const result = useMemo(() => {
-    return slowFunction(number)
+    return slowFunction(number) // Caches the return/result of a function
+  }, [number])
+
+  const sayHello = useCallback(() => {
+    return `Hello! Number is ${number}` // Caches the whole function
   }, [number])
 
   return (
     <div>
       <div>Hello!</div>
-      <Welcome firstname="Joe" lastname="Mo" />
+      <Welcome firstname="Joe" lastname="Smith" sayHello={sayHello} />
       <h2>Count: {count}</h2>
       <h2>Number: {number}</h2>
       <h2>Random: {result}</h2>
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,11 @@
-import { memo } from 'react'
+import { memo, useEffect } from 'react'
 
 const Welcome = memo(function Welcome(props) {
   console.log('Hello from Welcome component')
+
+  useEffect(() => {
+    console.log(props.sayHello())
+  }, [props])
 
   return (
     <h1>Welcome {props.firstname} {props.lastname}!</h1>
